b469bf1d603116cf90ee82fdc5efdf8fa9dde773
hpy <sandaoliu.123>
2021-12-10 09:01:53 +0800

    .

--- check_txt_flow.py
+++ check_txt_flow.py
@@ -4,13 +4,13 @@ import time
 
 seq_dir = os.path.expanduser('./')
 files = glob.glob(seq_dir + 'a.txt')
-flow_init = ''
+flow_init = []
 
 # 读取初始化的txt内容
 for f, file in enumerate(files):
     readfile = open(file, 'r')  # 读取文件
     fline = readfile.readlines()  # 读取txt文件中每一行
-    flow_init = fline
+    flow_init.append(fline)
     print(flow_init)
 
 while True:
